92839d70ff384d699af8c89c0ca71dd498f51e44
Rushikesh Patil <rushikesh.p@crestinfosystems.net>
2023-03-29 23:58:29 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import {
+  Button,
+  FormContainer,
+  Pre,
+  TextArea,
+  Title,
+  Wrapper,
+} from "./styles/styledComponet";
 function App() {
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 100,
+      });
+      console.log(response.data.choices);
+      setResult(response.data.choices[0].text);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <FormContainer>
+          <Title>ChatGPT</Title>
+          <TextArea
+            type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Write here"
+          />
+          <br />
+          <Button
+            disabled={loading || prompt.length === 0}
+            onClick={handleSubmit}
+          >
+            {loading ? "Generating..." : "Submit"}
+          </Button>
+          {result.length !== 0 && <Pre>{result}</Pre>}
+        </FormContainer>
+      </Wrapper>
     </div>
   );
 }
